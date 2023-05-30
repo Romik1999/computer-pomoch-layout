ac79574d12cd34d52ebdf30ea600b28bbd5de02f
roman <usankin.roma@gmail.com>
2023-05-30 20:49:20 +0400

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let burger = document.querySelector('.hamburger');
+    let mobile = document.querySelector('.mobile');
+    burger.addEventListener('click', () => {
+        if (burger.classList.contains('active')) {
+            burger.classList.remove('active');
+            mobile.classList.remove('active');
+            document.body.style.overflow = "visible";
+        } else {
+            burger.classList.add('active');
+            mobile.classList.add('active');
+            document.body.style.overflow = "hidden";
+        }
+    });
+
+});
