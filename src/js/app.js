7940d5dbbdea1bf5eab3229930ad430b28ef002b
Romik1999 <usankin.roma@gmail.com>
2023-05-31 17:39:08 +0400

    add font css

--- src/js/app.js
+++ src/js/app.js
@@ -13,4 +13,49 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+
+    const problemsSlider = new Swiper('.problems-slider', {
+        loop: false,
+        pagination: {
+            el: '.problems-slider__pagination',
+        },
+        navigation: {
+            nextEl: '.problems-slider__arrow--next',
+            prevEl: '.problems-slider__arrow--prev',
+        },
+        breakpoints: {
+            300: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+                grid: {
+                    rows: 2,
+                    fill: "row"
+                },
+            },
+            576: {
+                spaceBetween: 15,
+                slidesPerView: 2,
+                grid: {
+                    rows: 2,
+                    fill: "row"
+                },
+            },
+            769: {
+                spaceBetween: 15,
+                slidesPerView: 3,
+                grid: {
+                    rows: 2,
+                    fill: "row"
+                },
+            },
+            1024: {
+                spaceBetween: 24,
+                slidesPerView: 3,
+                grid: {
+                    rows: 3,
+                    fill: "row"
+                },
+            },
+        }
+    });
 });
